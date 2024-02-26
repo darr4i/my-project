c7d57c1b376f4a3e3836c113889470bbc5f230d2
Daria Rabiichuk <115396338+darr4i@users.noreply.github.com>
2024-02-27 00:22:29 +0200

    Update time.controller.go

--- controllers/time.controller.go
+++ controllers/time.controller.go
@@ -8,8 +8,8 @@ import (
 )
 
 func HandleTimeRequest(w http.ResponseWriter, r *http.Request) {
-    fmt.Println("Received time request")
-    fmt.Println("Request method:", r.Method)
+    fmt.Printf("Received time request\n")
+    fmt.Printf("Request method:", r.Method)
 	data := services.GetCurrentTime()
 	io.WriteString(w, string(data))
 }
